7b9d89a03ee6af8c00a85225c09db3f861f268cb
Skyzi000 <skyzi000r@gmail.com>
2021-11-25 20:52:35 +0900

    引数が多くなってきたので関数を一段下げ、ついでにマイルストーンをコピーできるようにした

--- src/execMkissue.ts
+++ src/execMkissue.ts
@@ -52,71 +52,102 @@ export function execMkissue(message: Message<boolean>): (...args: any[]) => Prom
     };
 
     async function makeIssues(sourceProjectId: string, destProjectId: string, execute: boolean, close: boolean, project?: string) {
-        const srcIssues = (await gitlab.Issues.all({ projectId: sourceProjectId, state: "opened" })) as IssueSchema[];
-        /** 生成元のマイルストーンのグローバルidをキー、生成先のマイルストーンのとする */
-        const milestoneDb = new Keyv(getKeyvPgConStr(), { table: `milestone_${sourceProjectId}`, namespace: destProjectId });
+        const srcIssues = (await gitlab.Issues.all({ projectId: sourceProjectId, state: "opened" }));
+        /** 生成元のマイルストーンのグローバルidをキー、生成先のマイルストーンのグローバルidとする */
+        const milestoneDb = new Keyv<number>(getKeyvPgConStr(), { table: `milestone_${sourceProjectId}`, namespace: destProjectId });
         /** 生成したIssueのiidをキー、生成元のIssueのiidをバリューとする */
-        const issueDb = new Keyv(getKeyvPgConStr(), { table: `issue_${sourceProjectId}`, namespace: destProjectId });
+        const issueDb = new Keyv<number>(getKeyvPgConStr(), { table: `issue_${sourceProjectId}`, namespace: destProjectId });
         const executionDetailHeader = `mkissue 実行内容${execute ? "" : "プレビュー"} (v${botVersion})\n\n発行元プロジェクトID: ${sourceProjectId}\n発行先プロジェクト: ${project} (id: ${destProjectId})\n`;
-        const results = await Promise.all(srcIssues.map(async srcIssue => makeIssue(srcIssue, destProjectId, execute, close, milestoneDb, issueDb)));
+        const results = await Promise.all(srcIssues.map(async srcIssue => makeIssue(srcIssue)));
         return `${executionDetailHeader}\n------\n${results.join("\n------\n")}`;
-    }
 
-    async function makeIssue(srcIssue: IssueSchema, destProjectId: string, execute: boolean, close: boolean, milestoneDb: Keyv, issueDb: Keyv): Promise<string> {
-        let executionDetailText = `Src: ${srcIssue.title} (#${srcIssue.iid})\n`;
-        try {
-            // チームごとのラベルは一旦外しておく
-            const labels = srcIssue.labels?.filter(name => !name.startsWith("2"));
-            if (labels?.find(l => l.match("個人"))) {
-                if (!labels.find(l => l.match("3_全役職"))) {
-                    if (labels.find(l => l.startsWith("3"))) {
-                        executionDetailText += `全役職対象でないミッション作成機能は未実装なのでスキップします\n`;
-                        return executionDetailText;
-                    }
-                    // 役職ラベルが何もついてなければ全役職ラベルを付ける
-                    labels.push("3_全役職");
+        async function makeIssue(sourceIssue: Omit<IssueSchema, "epic">): Promise<string> {
+            const srcIssue = sourceIssue as IssueSchema;
+            let executionDetailText = `Src: ${srcIssue.title} (#${srcIssue.iid})\n`;
+            try {
+                // チームごとのラベルは一旦外しておく
+                const labels = srcIssue.labels?.filter(name => !name.startsWith("2"));
+                // マイルストーンIDの取得
+                let destMilestoneId: number | undefined = await getDestMilestoneId(srcIssue);
+                if (srcIssue.milestone?.id && !destMilestoneId) {
+                    // マイルストーンを新規作成する
+                    const createdMilestone = await gitlab.ProjectMilestones.create(destProjectId, srcIssue.milestone.title, {
+                        description: srcIssue.milestone.description,
+                        due_date: srcIssue.milestone.due_date,
+                        start_date: srcIssue.milestone.start_date
+                    });
+                    destMilestoneId = createdMilestone.id;
+                    await milestoneDb.set(srcIssue.milestone.id.toString(), destMilestoneId);
                 }
-                const teamColors = await gitlabMemberTeamColors();
-                const destMembers = await gitlab.ProjectMembers.all(destProjectId, { includeInherited: true });
-                executionDetailText += `Labels: ${labels.join(", ")}\nTargetMembers: ${destMembers.map(m => m.name).join(", ")}\n`;
-                if (execute) {
-                    const results = await Promise.all(destMembers.map(async member => {
-                        try {
-                            const la = member.id in teamColors ? labels.concat([`2_${teamColors[member.id]}チーム`]) : labels;
-                            const created = await gitlab.Issues.create(destProjectId, {
-                                title: srcIssue.title,
-                                description: srcIssue.description,
-                                assignee_ids: [member.id],
-                                confidential: srcIssue.confidential,
-                                due_date: srcIssue.due_date,
-                                labels: la
-                            });
-                            issueDb.set(created.iid.toString(), srcIssue.iid);
-                            return created;
-                        } catch (error) {
-                            executionDetailText += `[Error] ${member.name}に割り当てるIssueの生成中にエラーが発生したのでスキップします\n詳細: ${error}`;
+                if (labels?.find(l => l.match("個人"))) {
+                    if (!labels.find(l => l.match("3_全役職"))) {
+                        if (labels.find(l => l.startsWith("3"))) {
+                            executionDetailText += `全役職対象でないミッション作成機能は未実装なのでスキップします\n`;
+                            return executionDetailText;
                         }
-                    }));
-                    executionDetailText += `生成したIssueの数: ${results.filter(r => r !== undefined).length}`;
-                }
-                if (close) {
-                    executionDetailText += `ソースプロジェクトの ${srcIssue.title} (#${srcIssue.iid}) をClose\n`;
-                    if (execute) {
-                        gitlab.Issues.closedBy(srcIssue.project_id, srcIssue.iid);
+                        // 役職ラベルが何もついてなければ全役職ラベルを付ける
+                        labels.push("3_全役職");
                     }
+                    const teamColors = await gitlabMemberTeamColors();
+                    const destMembers = await gitlab.ProjectMembers.all(destProjectId, { includeInherited: true });
+                    executionDetailText += `Labels: ${labels.join(", ")}\nTargetMembers: ${destMembers.map(m => m.name).join(", ")}\n`;
+                    if (execute) {
+                        const results = await Promise.all(destMembers.map(async member => {
+                            try {
+                                const la = member.id in teamColors ? labels.concat([`2_${teamColors[member.id]}チーム`]) : labels;
+                                const created = await gitlab.Issues.create(destProjectId, {
+                                    title: srcIssue.title,
+                                    description: srcIssue.description,
+                                    assignee_ids: [member.id],
+                                    confidential: srcIssue.confidential,
+                                    due_date: srcIssue.due_date,
+                                    labels: la,
+                                    milestone_id: destMilestoneId
+                                });
+                                issueDb.set(created.iid.toString(), srcIssue.iid);
+                                return created;
+                            } catch (error) {
+                                executionDetailText += `[Error] ${member.name}に割り当てるIssueの生成中にエラーが発生したのでスキップします\n詳細: ${error}`;
+                            }
+                        }));
+                        executionDetailText += `生成したIssueの数: ${results.filter(r => r !== undefined).length}`;
+                    }
+                    if (close) {
+                        executionDetailText += `ソースプロジェクトの ${srcIssue.title} (#${srcIssue.iid}) をClose\n`;
+                        if (execute) {
+                            gitlab.Issues.closedBy(srcIssue.project_id, srcIssue.iid);
+                        }
+                    }
+                } else if (srcIssue.labels?.find(l => l.match("グループ"))) {
+                    executionDetailText += `グループミッション作成機能は未実装なのでスキップします\n`;
+                    return executionDetailText;
+                } else {
+                    executionDetailText += `[Warn] 個人ラベルもグループラベルも付いていないのでスキップします\n`;
+                    return executionDetailText;
                 }
-            } else if (srcIssue.labels?.find(l => l.match("グループ"))) {
-                executionDetailText += `グループミッション作成機能は未実装なのでスキップします\n`;
-                return executionDetailText;
-            } else {
-                executionDetailText += `[Warn] 個人ラベルもグループラベルも付いていないのでスキップします\n`;
+            } catch (error) {
+                executionDetailText += `[Error] 予期しないエラーが発生したのでスキップします\n詳細: ${error}`;
                 return executionDetailText;
             }
-        } catch (error) {
-            executionDetailText += `[Error] 予期しないエラーが発生したのでスキップします\n詳細: ${error}`;
             return executionDetailText;
         }
-        return executionDetailText;
+
+        async function getDestMilestoneId(srcIssue: IssueSchema) {
+            if (!srcIssue.milestone)
+                return undefined;
+            // まずデータベースから取得を試みる
+            let milestoneId = await milestoneDb.get(srcIssue.milestone.id.toString());
+            if (!milestoneId || isNaN(milestoneId))
+                return milestoneId;
+            // データベースになければTitleをもとに検索する
+            const destMilestones = await gitlab.ProjectMilestones.all(destProjectId);
+            milestoneId = destMilestones.find(milestone => { milestone.title === srcIssue.milestone.title })?.id;
+            if (!milestoneId)
+                return undefined;
+            // 見つかったらデータベースにidを書き込んでから返す
+            await milestoneDb.set(srcIssue.milestone.id.toString(), milestoneId);
+            return milestoneId;
+        }
     }
 
     async function replyFile(data: string, extension: string, project?: string) {
